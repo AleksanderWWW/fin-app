293d1842cf97c87da0b55ca0013b3ae7315f7778
AleksanderWWW <alwojnarowicz@gmail.com>
2023-07-12 09:03:46 +0200

    use gorilla logging handler

--- api/main.go
+++ api/main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"context"
-    "flag"
-    "log"
-    "net/http"
-    "os"
-    "os/signal"
-    "time"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/AleksanderWWW/fin-app/app"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return handlers.LoggingHandler(os.Stdout, next)
+}
 
 func main() {
 	listenAddr := flag.String("listenaddr", ":5555", "api port to listen on")
 	wait := flag.Duration(
-		"graceful-timeout", 
-		time.Second * 15, 
+		"graceful-timeout",
+		time.Second*15,
 		"the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m",
 	)
 
@@ -26,44 +30,45 @@ func main() {
 
 	r := mux.NewRouter().StrictSlash(true)
 	r.Use(mux.CORSMethodMiddleware(r))
+	r.Use(loggingMiddleware)
 	apiV1Router := r.PathPrefix("/api/v1").Subrouter()
 
 	apiV1Router.HandleFunc("/{provider}", app.HandleRequest).Methods(http.MethodPost)
 
 	srv := &http.Server{
-        Addr:         *listenAddr,
-        // Good practice to set timeouts to avoid Slowloris attacks.
-        WriteTimeout: time.Second * 15,
-        ReadTimeout:  time.Second * 15,
-        IdleTimeout:  time.Second * 60,
-        Handler: r, // Pass our instance of gorilla/mux in.
-    }
+		Addr: *listenAddr,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
 
-    // Run our server in a goroutine so that it doesn't block.
-    go func() {
-        if err := srv.ListenAndServe(); err != nil {
-            log.Println(err)
-        }
-    }()
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
 
-    c := make(chan os.Signal, 1)
-    // We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-    // SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-    signal.Notify(c, os.Interrupt)
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
 
-    // Block until we receive our signal.
-    <-c
+	// Block until we receive our signal.
+	<-c
 
-    // Create a deadline to wait for.
-    ctx, cancel := context.WithTimeout(context.Background(), *wait)
-    defer cancel()
-    // Doesn't block if no connections, but will otherwise wait
-    // until the timeout deadline.
-    srv.Shutdown(ctx)
-    // Optionally, you could run srv.Shutdown in a goroutine and block on
-    // <-ctx.Done() if your application should wait for other services
-    // to finalize based on context cancellation.
-    log.Println("shutting down")
-    os.Exit(0)
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), *wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
 
 }
