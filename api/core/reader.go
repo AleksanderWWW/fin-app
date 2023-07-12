f04033704f52f998544b3f8d0b38a30b0ee8404f
AleksanderWWW <alwojnarowicz@gmail.com>
2023-07-12 09:04:47 +0200

    reader getter returns a reader and error

--- api/core/reader.go
+++ api/core/reader.go
@@ -1,14 +1,29 @@
 package core
 
 import "time"
-
+import "fmt"
 
 type Record struct {
-	Date time.Time `json:"date"`
-	Open float32 `json:"open"`
-	Close float32 `json:"close"`
+	Date  time.Time `json:"date"`
+	Open  float32   `json:"open"`
+	Close float32   `json:"close"`
 }
 
 type Reader interface {
 	FetchData() []Record
+}
+
+func GetReaderFromProviderString(provider string, initArgs any) (Reader, error) {
+	initArgsMap, ok := initArgs.(map[string]interface{})
+
+	if !ok {
+		return nil, fmt.Errorf("Malformed arguments %v", initArgs)
+	}
+
+	switch provider {
+	case "mock":
+		return &MockReader{initArgsMap}, nil
+	default:
+		return nil, fmt.Errorf("Invalid data provider - '%s'", provider)
+	}
 }
