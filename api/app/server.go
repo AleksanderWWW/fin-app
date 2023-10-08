b8a0ab0c1aab60289e17111d683dabed5161a038
AleksanderWWW <alwojnarowicz@gmail.com>
2023-10-08 15:25:24 +0200

    add headers

--- api/app/server.go
+++ api/app/server.go
@@ -10,6 +10,9 @@ import (
 
 func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 	// -------------------------------------------------------------------------
 	// Parse request
